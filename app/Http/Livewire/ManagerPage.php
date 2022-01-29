d83200a19b6a275ccc6fc806da5fc2792871c5e1
Captain Burah <cptburah@gmail.com>
2022-01-29 19:52:48 +0530

    2nd | configs

---
+++ app/Http/Livewire/ManagerPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ManagerPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.manager-page');
+    }
+}
