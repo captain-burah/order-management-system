24b84dfe9eebc68621609184329f59138b5c6d09
Captain Burah <cptburah@gmail.com>
2022-02-23 16:04:24 +0530

    Manager

--- app/Http/Livewire/ManagerPage.php
+++ app/Http/Livewire/ManagerPage.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Manager;
 use Livewire\Component;
 
 class ManagerPage extends Component
 {
+
+    public $name;
+    public $email;
+    public $modalFormVisible = false;
+
+    public function createShowModal() 
+    {
+        $this->modalFormVisible = true;
+    }
     public function render()
     {
         return view('livewire.manager-page');
